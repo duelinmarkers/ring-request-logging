46c55f59bb5145d8b7dd2082bac5cdf797936195
John Hume <duelin.markers@gmail.com>
2012-11-02 16:51:52 -0500

    Release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.duelinmarkers/ring-request-logging "0.1.2-SNAPSHOT"
+(defproject com.duelinmarkers/ring-request-logging "0.1.2"
   :description "Ring middleware to log requests and responses"
   :url "http://github.com/duelinmarkers/ring-request-logging"
   :license {:name "Eclipse Public License"
